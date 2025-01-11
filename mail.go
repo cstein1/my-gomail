87602c99659aaf636f20925255dcc8421f8bd11a
cstein1 <cstein.code@gmail.com>
2025-01-11 14:57:53 -0600

    first commit

---
+++ mail.go
@@ -0,0 +1,135 @@
+package mygomail
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/google/uuid"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/gmail/v1"
+	"google.golang.org/api/option"
+)
+
+/*
+resources:
+https://developers.google.com/gmail/api/reference/rest/v1/users.messages
+https://github.com/googleworkspace/go-samples/blob/master/gmail/quickstart/quickstart.go
+*/
+
+// Retrieve a token, saves the token, then returns the generated client.
+func getClient(config *oauth2.Config) *http.Client {
+	// The file token.json stores the user's access and refresh tokens, and is
+	// created automatically when the authorization flow completes for the first
+	// time.
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline, oauth2.AccessTypeOnline, oauth2.ApprovalForce)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code: %v", err)
+	}
+
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web: %v", err)
+	}
+	return tok
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
+
+func SendMail(from, to, subject, msg, credentialsPath string) {
+	ctx := context.Background()
+	b, err := os.ReadFile(credentialsPath)
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// If modifying these scopes, delete your previously saved token.json.
+	config, err := google.ConfigFromJSON(b, gmail.GmailSendScope, gmail.GmailComposeScope, gmail.GmailModifyScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(config)
+
+	srv, err := gmail.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Gmail client: %v", err)
+	}
+
+	msgString := fmt.Sprintf("From: %s\n", from)
+	msgString += fmt.Sprintf("To: %s\n", to)
+	msgString += fmt.Sprintf("Subject: %s\n", subject)
+	msgString += fmt.Sprintf("Date: %s\n", time.Now())
+	msgString += fmt.Sprintf("Message-ID: %s\n", uuid.NewString())
+	msgString += fmt.Sprint(msg)
+
+	var message *gmail.Message = &gmail.Message{
+		Raw: base64.URLEncoding.EncodeToString([]byte(msgString)),
+		Payload: &gmail.MessagePart{
+			Body: &gmail.MessagePartBody{
+				Data: base64.URLEncoding.EncodeToString([]byte(msgString)),
+			},
+			Headers: []*gmail.MessagePartHeader{
+				{
+					Name:  "To",
+					Value: to,
+				},
+				{
+					Name:  "From",
+					Value: from,
+				},
+				{
+					Name:  "Subject",
+					Value: subject,
+				},
+			},
+		},
+	}
+	gmsg, err := srv.Users.Messages.Send("me", message).Do()
+	if err != nil {
+		fmt.Printf("Err: %v\n", err.Error())
+	} else {
+		fmt.Printf("Status code: %v\n", gmsg.HTTPStatusCode)
+	}
+}
